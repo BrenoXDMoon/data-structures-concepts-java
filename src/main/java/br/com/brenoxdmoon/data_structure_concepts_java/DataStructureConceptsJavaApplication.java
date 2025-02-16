934375d520086a2071b6cea36f2d8ca0b187d320
Breno Gabriel <brenogabriel19@gmail.com>
2025-02-16 18:26:10 -0300

    Initial commit

---
+++ src/main/java/br/com/brenoxdmoon/data_structure_concepts_java/DataStructureConceptsJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.brenoxdmoon.data_structure_concepts_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureConceptsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureConceptsJavaApplication.class, args);
+	}
+
+}
